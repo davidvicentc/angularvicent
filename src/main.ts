e0aa7c10bc14b1eacdbc62a4a47c8e4fc3dc0d4e
David Vicent <djassiell@gmail.com>
2025-02-04 10:23:16 -0400

    Build for GitHub Pages

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes, withHashLocation()), provideAnimations()],
+  providers: [...appConfig.providers, provideAnimations()],
 }).catch((err) => console.error(err));
