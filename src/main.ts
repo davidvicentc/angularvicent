19f387fa7b372f343ed9ac16a3b259fef03bc8af
David Vicent <djassiell@gmail.com>
2025-02-04 10:17:36 -0400

    Switch to HashLocationStrategy for better GitHub Pages compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [...appConfig.providers, provideAnimations()],
+  providers: [provideRouter(routes, withHashLocation()), provideAnimations()],
 }).catch((err) => console.error(err));
